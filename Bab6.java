e02d19bdeaa2762e8ac27c5567be43130d60c816
salwaall <putrisalwaalthoffunnisa@gmail.com>
2025-08-20 08:37:43 +0700

    Add files via upload

---
+++ Bab6.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab.pkg6;
+
+/**
+ *
+ * @author salwa
+ */
+public class Bab6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
