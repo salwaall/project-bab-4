a882794759ec3db702c1f8a8ac082c312bf92aa5
salwaall <putrisalwaalthoffunnisa@gmail.com>
2025-08-20 08:40:18 +0700

    Add files via upload

---
+++ projectsalwa.java
@@ -0,0 +1,179 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyekbaru;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+
+public class projectsalwa {
+
+    private static String variabel1;
+    private static String variabel2;
+    private static String var1;
+    private static String var2;
+    private static String var3;
+    public static void main(String[] args) throws IOException {
+//      System.out.println("hello world");
+//      System.out.println("Halo"+" "+"Dunia");
+//      System.out.println(2+" "+5);
+//      System.out.println("Angka "+" "+5);
+//      System.out.println("2 + 5"+" = "+2+5);  
+//      System.out.println("2 + 5"+" = "+(2+5));
+//      System.out.println("2 + 2" + (2+2));
+
+                
+
+
+//        String namaDepan = "";
+//        String namaBelakang = "";
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.print("Nama depan: ");
+//        namaDepan = input.next();
+//
+//        System.out.print("Nama belakang: ");
+//        namaBelakang = input.next();
+//
+//        System.out.println("Nama saya adalah: " + namaDepan + " " + namaBelakang);
+
+
+
+//        String namaDepan = "";
+//        String namaBelakang = "";
+//
+//        
+//        InputStreamReader ireader = new InputStreamReader(System.in);
+//        
+//        BufferedReader breader = new BufferedReader(ireader);
+//
+//        System.out.println("Nama Depan : ");
+//        
+//        namaDepan = breader.readLine();
+//
+//        System.out.println("Nama Belakang : ");
+//        
+//        namaBelakang = breader.readLine();
+//
+//        System.out.println("Nama Saya adalah :");
+//        System.out.println(namaDepan + " " + namaBelakang);
+
+    
+//  { String nama =
+//JOptionPane.showInputDialog("Masukan Nama :");
+//System.out.println("Nama : "+nama);}
+
+
+//String a =
+//JOptionPane.showInputDialog("Masukan nilaiuntuk variabel a : ");
+//
+//String b =
+//JOptionPane.showInputDialog("Masukan nilaiuntuk variabel b : ");
+//
+//System.out.println("Hasil :" +
+//(Integer.parseInt(a) *
+//Integer.parseInt(b)));
+
+//String nama = "Salwa";
+//String jurusan = "PPLG";
+//JOptionPane.showMessageDialog(null, "Halo " + nama
+//+ ",jurusan Kamu " + jurusan);
+
+//JOptionPane.showMessageDialog(
+//null, "Sebuah Informasi.","Informasi",
+//JOptionPane.INFORMATION_MESSAGE);
+
+//JOptionPane.showMessageDialog(
+//null, "Sebuah Error !","Error",
+//JOptionPane.ERROR_MESSAGE);
+
+
+//JOptionPane.showMessageDialog(
+//null, "Sebuah Peringatan.","Peringatan",
+//JOptionPane.WARNING_MESSAGE);
+
+//JOptionPane.showMessageDialog(
+//null, "Sebuah Pertanyaan.", "Pertanyaan",
+//JOptionPane.QUESTION_MESSAGE);
+
+//JOptionPane.showMessageDialog(
+//null, "Sebuah Pertanyaan.", "Pertanyaan",
+//JOptionPane.QUESTION_MESSAGE);
+
+
+//JOptionPane.showConfirmDialog(null, "Pilih Yes atau No?", "Pertanyaan",JOptionPane.YES_NO_OPTION);
+//        int pertanyaan = 0;
+//if(JOptionPane.YES_OPTION == pertanyaan){
+//JOptionPane.showMessageDialog(null, "Kamu memilih Yes.");
+//}else if(JOptionPane.NO_OPTION == pertanyaan){
+//JOptionPane.showMessageDialog(null, "Kamu memilih No.");}}
+
+//System.out.println("variabel1 -> " + variabel1);
+//System.out.println("variabel2 -> " + variabel2);
+    
+//System.out.println("multi deklarasi -> " + " " + var1);
+//System.out.println("multi deklarasi -> " + " " + var2);
+//System.out.println("multi deklarasi -> " + " " + var3);
+
+//System.out.println("variabel1 -> " + variabel1);
+//System.out.println("variabel2 -> " + variabel2);
+//System.out.println("setelah diisi :");
+//variabel1 = "Hello";
+//variabel2 = "Word";
+//System.out.println("variabel1 -> " + variabel1);
+//System.out.println("variabel2 -> " + variabel2);
+
+//String var1 = "1234567899";
+//String var2 = "3.14";
+//String var3 = "1";
+//
+//int bulat = Integer.parseInt(var1);
+//double pecahan = Double.parseDouble(var2);
+//short bulat2 = Short.parseShort(var3);
+//
+//System.out.println("variabel var1 " + var1);
+//System.out.println("variabel var2 " + var2);
+//System.out.println("variabel var3 " + var3);
+//System.out.println("---");
+//System.out.println("konversi bilangan bulat " + bulat);
+//System.out.println("konversi bilangan pecahan " + pecahan);
+//System.out.println("konversi bilangan bulat2 " + bulat2);
+
+//int var1 = 5;
+//int var2 = 3;
+//
+//System.out.println("Hasil : "+ var1 / var2);
+
+//int var1 = 5;
+//int var2 = 3;
+//
+//System.out.println("Hasil : "+ (double)var1 /
+//var2);
+
+//Integer sebelum = 20;
+//Integer sesudah = 0;
+//System.out.println("Sebelum Increment : " + sebelum);
+//sesudah = ++sebelum;
+//System.out.println("Sesudah Increment : " + sesudah);
+
+//Integer sebelum = 9;
+//Integer sesudah = 0;
+//
+//System.out.println("Sebelum Decrement : " + sebelum);
+//sesudah = --sebelum;
+//System.out.println("Sesudah Decrement : " + sesudah);
+
+//int a = 2;
+//int b = 0;
+//
+//System.out.println("Sebelum negasi : " + a);
+//b = -a;
+//System.out.println("Sebelum negasi : " + b);
+
+
+    } 
+    }
